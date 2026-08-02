b4addcfdc270e0e232cd7f9461478c44f7654a65
Zakir Hussain Syed <zakirhussainsyed4@gmail.com>
2026-08-01 23:21:42 -0700

    Add Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-zkc7j@1-b0aat!%ee09@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0,tellermedia.onrender.com').split(',')
 
 
 # Application definition
