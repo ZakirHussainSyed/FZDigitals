5959eb42c39a2910d128fd9130611585d3cd6446
Zakir Hussain Syed <zakirhussainsyed4@gmail.com>
2026-08-05 01:58:26 -0700

    Remove static file serving to allow custom media view

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('slideshow.urls')),
 ]
-
-# Serve media files in production (for Render deployment)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
