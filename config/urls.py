ace3259843f0d11c55c3efee1d8af0410ff56a53
Zakir Hussain Syed <zakirhussainsyed4@gmail.com>
2026-08-09 16:57:40 -0700

    Remove custom media serving since S3 handles file serving

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from slideshow import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('slideshow.urls')),
-    path('media/<path:path>', views.serve_media, name='serve-media'),
 ]
