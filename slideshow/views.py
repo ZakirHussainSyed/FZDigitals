31444b26560315a45cd9a3bc2882047b51f15e61
Zakir Hussain Syed <zakirhussainsyed4@gmail.com>
2026-08-20 19:45:33 -0700

    Fix signup authentication backend error

--- slideshow/views.py
+++ slideshow/views.py
@@ -84,7 +84,9 @@ def signup(request):
             security_question=security_question,
             security_answer=security_answer.lower()
         )
-        login(request, user)
+        from django.contrib.auth import get_backends
+        backend = get_backends()[0]  # Use the first available backend
+        login(request, user, backend=backend)
         return redirect(f'/{user.id}/')
     
     return render(request, 'slideshow/signup.html', {
