ac91235b7126d3dddf480544da08b85a352a727a
Zakir Hussain Syed <zakirhussainsyed4@gmail.com>
2026-08-22 12:45:34 -0700

    Add allauth social account provider settings for Google OAuth

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,19 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Social account settings
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'offline',
+        }
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
