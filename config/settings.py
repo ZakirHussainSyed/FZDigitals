cb693ecb51331eb45ed417871604c9073e64148d
Zakir Hussain Syed <zakirhussainsyed4@gmail.com>
2026-09-10 15:26:14 -0700

    Disable S3 object ACLs; bucket uses policy-based public access

--- config/settings.py
+++ config/settings.py
@@ -192,7 +192,7 @@ if os.environ.get('AWS_STORAGE_BUCKET_NAME'):
     
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None  # bucket has ACLs disabled; public access is via bucket policy
     AWS_QUERYSTRING_AUTH = False
     
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
