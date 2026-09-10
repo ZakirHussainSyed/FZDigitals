c13dabae7a67cb13dc3c441c356e9221df2588a4
Zakir Hussain Syed <zakirhussainsyed4@gmail.com>
2026-09-10 11:51:43 -0700

    Fix tablet video playback: robust autoplay, public S3 URLs

--- config/settings.py
+++ config/settings.py
@@ -190,8 +190,10 @@ if os.environ.get('AWS_STORAGE_BUCKET_NAME'):
     AWS_S3_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
     AWS_S3_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
     
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_QUERYSTRING_AUTH = False
     
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
